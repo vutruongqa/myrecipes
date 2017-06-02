ab4c4ac6893f48b69e7aaf912476902b8fe3e14f
Vu Truong <vutruongqa@gmail.com>
2017-06-02 20:54:35 +0700

    add one to many relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602105719) do
+ActiveRecord::Schema.define(version: 20170602112538) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170602105719) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "chef_id"
   end
 
 end
