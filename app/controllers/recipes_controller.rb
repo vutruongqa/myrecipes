46086b0984bec272f18a5a0ae149b07f1962f4e2
Vu Truong <vutruongqa@gmail.com>
2017-06-05 15:55:59 +0700

    add delete function

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,14 +24,24 @@ class RecipesController < ApplicationController
   end
 
   def edit
-
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
-
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    flash[:success] = "Recipe was removed successfully!"
+    redirect_to recipes_path
 
   end
 
