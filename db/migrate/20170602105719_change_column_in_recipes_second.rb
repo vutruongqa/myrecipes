7c5fd962847a95fa51ca46e468cba5c4c65bd906
Vu Truong <vutruongqa@gmail.com>
2017-06-05 16:09:24 +0700

    update database

--- db/migrate/20170602105719_change_column_in_recipes_second.rb
+++ db/migrate/20170602105719_change_column_in_recipes_second.rb
@@ -1,6 +1,6 @@
 class ChangeColumnInRecipesSecond < ActiveRecord::Migration[5.0]
   def change
-    rename_column :recipes, :text, :description
+    #rename_column :recipes, :text, :description
     change_column :recipes, :description, :text
   end
 end
