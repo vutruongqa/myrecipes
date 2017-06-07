2c6fb4745aa81611180751e24a947de0dc8ee610
Vu Truong <vutruongqa@gmail.com>
2017-06-07 17:12:22 +0700

    add model for comment

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+class Comment < ApplicationRecord
+  validates :description, presence: true, length: {:minimum => 4, :maximum => 40}
+  belongs_to :chef
+  belongs_to :recipe
+  validates :chef_id, presence: true
+  validate :recipe_id, presence: true
+  default_scope -> {order(updated_at: :desc)}
+
+end
