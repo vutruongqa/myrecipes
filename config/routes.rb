1d7c92dc7bc788b257ec8f022cc2ea01c2e76d4e
Vu Truong <vutruongqa@gmail.com>
2017-06-11 21:27:48 +0700

    add missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   #mount ActionCable.server => '/cable'
 
-  get '/chat', to: 'chatrooms#show'
+  #get '/chat', to: 'chatrooms#show'
 
   resources :messages, only: [:create]
 
