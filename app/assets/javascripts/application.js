4a80c088af91f4a10ed22881417b6a841638eafd
Vu Truong <vutruongqa@gmail.com>
2017-06-15 16:05:15 +0700

    fix auto-suggestion

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,7 +37,7 @@ function scrollToBottom(){
 
 $(document).ready(function(){
     scrollToBottom();
-    $('#the-basics .typeahead').change(function() {
+    $(document).on('change','#the-basics .typeahead', function() {
         /* This will be fired every time, when textbox's value changes. */
         if ($('#the-basics .typeahead').val().length > 2) {
             var id_numbers = new Array();
